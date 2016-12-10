0d86d5b79fe42e115a04898851546cf39cf46aad
Stefan Groth <stefangroth@hotmail.com>
2016-12-10 18:25:27 +0100

    avatars added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,10 @@ import { Dude } from './dude-details/dude-details';
 })
 export class AppComponent {
   title = 'app wor!';
-  dudes: Dude[] = [new Dude('Bene', 10), new Dude('Falk', 10.2), new Dude('Consti', 7), new Dude('Stefan', 2.4)];
+  dudes: Dude[] = [new Dude('Bene', 10, 'bene_avatar.png'),
+                   new Dude('Falk', 10.2, 'falk_avatar.jpg'),
+                   new Dude('Consti', 7, 'consti_avatar.jpg'),
+                   new Dude('Stefan', 2.4)];
 
   currentDude: Dude = this.dudes[0];
 }
