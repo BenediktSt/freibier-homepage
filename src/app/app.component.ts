6b4ddebae1b54a3fb4cd4f507872ba170390b792
Stefan Groth <s_groth@hotmail.com>
2017-09-21 21:05:24 +0200

    Elvish mystic component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,29 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+<<<<<<< HEAD
+=======
+  title = 'Api Response';
+  private apiUrl = 'https://api.magicthegathering.io/v1/cards';
+  data : any = {};
+
+  constructor(private http : Http) {
+    this.getContacts();
+    this.getData();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl, {
+      params : { name : 'Elvish Mystic' }
+    }).map((res : Response) => res.json())
+  }
+
+  getContacts() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;} )
+  }
+
+
+>>>>>>> Elvish mystic component
 }
