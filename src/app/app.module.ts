4a5f77415c8a6198d13c767ebcdcfb987c70f7a9
Stefan Groth <s_groth@hotmail.com>
2017-09-21 21:05:24 +0200

    Elvish mystic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HttpModule } from '@angular/http';
+
 import { MaterialModule } from './material.module';
 import { appRouts } from './routes';
 
@@ -18,7 +20,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     RouterModule.forRoot(appRouts),
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
