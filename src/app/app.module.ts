45f5deea4368b54736980b4cbece3c78d04cfa90
Stefan Groth <s_groth@hotmail.com>
2017-09-28 20:06:08 +0200

    Working on search field for card names.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { HttpModule } from '@angular/http';
 
@@ -8,6 +9,7 @@ import { MaterialModule } from './material.module';
 import { appRouts } from './routes';
 
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { DudeAdminComponent } from './dude-admin/dude-admin.component';
 import { HomeComponent } from './home/home.component';
 
@@ -16,12 +18,21 @@ import { HomeComponent } from './home/home.component';
       AppComponent,
       DudeAdminComponent,
       HomeComponent
+=======
+import { CardSearchComponent } from './card-search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardSearchComponent
+>>>>>>> Working on search field for card names.
   ],
   imports: [
     RouterModule.forRoot(appRouts),
     BrowserModule,
     MaterialModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
