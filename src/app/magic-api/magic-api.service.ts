f2db018dbd1548447d49aae92e1aad574301b95e
Stefan Groth <s_groth@hotmail.com>
2017-10-12 17:37:08 +0200

    implemented magic api service

---
+++ src/app/magic-api/magic-api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class MagicApiService {
+  private apiUrl = 'https://api.magicthegathering.io/v1/cards';
+
+  constructor(private http: Http) { }
+
+  getCards(restrictions) {
+    return this.http.get(this.apiUrl, restrictions).map((res: Response) => res.json());
+  }
+
+}
