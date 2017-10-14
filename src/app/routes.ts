17a71bf675a18e3310cb1f7d3c0d4c55b3aeba4b
Stefan Groth <s_groth@hotmail.com>
2017-10-14 21:30:35 +0200

    ROUTER

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { DudeAdminComponent } from './dude-admin/dude-admin.component';
+import { StefanComponent } from './stefan/stefan.component';
 
 export const appRouts: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'dudes', component: DudeAdminComponent },
+    { path: 'stefan', component: StefanComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
   ];
