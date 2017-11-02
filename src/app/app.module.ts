17d0676c260452b7f552b36e126fb7b0f89eb33b
Stefan Groth <s_groth@hotmail.com>
2017-11-02 20:24:59 +0100

    Fixed rebasing bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,25 +15,9 @@ import { SearchFieldComponent } from './search-field/search-field.component';
 import { CardPrintingsComponent } from './card-printings/card-printings.component';
 import { CardRatingComponent } from './card-rating/card-rating.component';
 import { CardDetailsComponent } from './card-details/card-details.component';
-<<<<<<< HEAD
-import {StefanComponent} from './stefan/stefan.component';
-import { LinkhubComponent } from './linkhub/linkhub.component';
 import { CardSearchComponent } from './card-search/card-search.component';
-=======
 import { StefanComponent } from './stefan/stefan.component';
-<<<<<<< HEAD
->>>>>>> Implemented a class for magic cards so we have a clean interface for them. Also implemented a cardParser that parses the json given from the api and returns a list of MagicCards
-=======
-import { CardSearchComponent } from './card-search/card-search.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> Decoupled card search from card details. Search now is able to discern if a given card name refers to a specific card
-=======
 import { LinkhubComponent } from './linkhub/linkhub.component';
->>>>>>> Rebase 4/6
-=======
-import { LinkhubComponent } from './linkhub/linkhub.component';
->>>>>>> merge conflict
 
 @NgModule({
   declarations: [
@@ -44,23 +28,9 @@ import { LinkhubComponent } from './linkhub/linkhub.component';
       CardRatingComponent,
       CardDetailsComponent,
       CardPrintingsComponent,
-<<<<<<< HEAD
       StefanComponent,
-<<<<<<< HEAD
       LinkhubComponent,
       CardSearchComponent
-=======
-      CardSearchComponent,
-<<<<<<< HEAD
-      StefanComponent
->>>>>>> Decoupled card search from card details. Search now is able to discern if a given card name refers to a specific card
-=======
-      StefanComponent,
-      LinkhubComponent
->>>>>>> Rebase 4/6
-=======
-      LinkhubComponent
->>>>>>> merge conflict
   ],
   imports: [
     RouterModule.forRoot(appRouts),
